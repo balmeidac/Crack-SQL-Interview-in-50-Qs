ec21d967b35ec4b7e921dcc5ded9c7d850265bfc
Byron Omar Almeida Coello <byronalmeidaec@gmail.com>
2024-09-05 15:39:59 -0500

    Update Q1-Recyclable-and-Low-Fat-Products.sql

--- Select/Q1-Recyclable-and-Low-Fat-Products.sql
+++ Select/Q1-Recyclable-and-Low-Fat-Products.sql
@@ -1,4 +1,4 @@
---  QUESTION 1 1757. Recyclable and Low Fat Products
+--  QUESTION 1 Recyclable and Low Fat Products
 --  Table: Products
 
 --  +-------------+---------+
@@ -12,7 +12,7 @@
 --  low_fats is an ENUM (category) of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
 --  recyclable is an ENUM (category) of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
 
-
+-- QUESTION 1
 -- Write a solution to find the ids of products that are both low fat and recyclable.
 -- Return the result table in any order.
 
@@ -40,8 +40,18 @@
 -- +-------------+
 -- Explanation: Only products 1 and 3 are both low fat and recyclable.
 
-/* Write your T-SQL query statement below */
+-- ANSWER 1
+-- Write a solution to find the ids of products that are both low fat and recyclable.
+-- Return the result table in any order.
+
+-- Select the product_id of products that meet the conditions
 SELECT product_id
+
+-- The data is selected from the products table
 FROM products
+
+-- Filter the rows where the low_fats column has a value of 'Y' (Yes, low fats)
 WHERE low_fats = 'Y'
+
+-- Additionally, filter the rows where the recyclable column also has a value of 'Y' (Yes, recyclable)
 AND recyclable = 'Y';
