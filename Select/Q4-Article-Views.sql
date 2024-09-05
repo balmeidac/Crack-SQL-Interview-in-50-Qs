270cf11ef9031f9092465662ce7b804661f0f372
Byron Omar Almeida Coello <byronalmeidaec@gmail.com>
2024-09-05 15:36:05 -0500

    Create Q4-Article-Views.sql

---
+++ Select/Q4-Article-Views.sql
@@ -0,0 +1,62 @@
+/* QUESTION 4 Article Views 
+
+Table: Views
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key (column with unique values) for this table, the table may have duplicate rows.
+Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+Note that equal author_id and viewer_id indicate the same person.
+ 
+
+Write a solution to find all the authors that viewed at least one of their own articles.
+
+Return the result table sorted by id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Views table:
++------------+-----------+-----------+------------+
+| article_id | author_id | viewer_id | view_date  |
++------------+-----------+-----------+------------+
+| 1          | 3         | 5         | 2019-08-01 |
+| 1          | 3         | 6         | 2019-08-02 |
+| 2          | 7         | 7         | 2019-08-01 |
+| 2          | 7         | 6         | 2019-08-02 |
+| 4          | 7         | 1         | 2019-07-22 |
+| 3          | 4         | 4         | 2019-07-21 |
+| 3          | 4         | 4         | 2019-07-21 |
++------------+-----------+-----------+------------+
+Output: 
++------+
+| id   |
++------+
+| 4    |
+| 7    |
++------+ */
+
+
+-- ANSWER
+-- Select distinct author_id and rename it as id for the output
+SELECT DISTINCT author_id AS id
+
+-- The data is selected from the Views table
+FROM Views
+
+-- Filter the rows where the author_id is equal to the viewer_id
+WHERE author_id = viewer_id
+
+-- Sort the output by id in ascending order
+ORDER BY id ASC;
+
